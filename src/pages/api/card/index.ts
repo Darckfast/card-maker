cb61ad488cb7879deb3da581dec7b0c89592cfbd
Costa <victorcosta37@gmail.com>
2021-06-12 00:09:40 -0300

    chore: add api route

---
+++ src/pages/api/card/index.ts
@@ -0,0 +1,96 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { ServerStyleSheet } from 'styled-components'
+import { renderToString } from 'react-dom/server'
+import Card from '../../../components/card/Card'
+import { get } from 'https'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const {
+    name,
+    description,
+    imgSrc,
+    holoEffect,
+    sparklesEffect,
+    backgroundColor,
+    innerBackgroundColor
+  } = req.query
+
+  const configs = {
+    configs: {
+      name,
+      description,
+      imgSrc: await getImage(imgSrc as string),
+      holoEffect: holoEffect === 'true',
+      sparklesEffect: sparklesEffect === 'true',
+      backgroundColor,
+      innerBackgroundColor,
+      enableAnimation: true,
+      holoPosition: {
+        position: {
+          x: '50%',
+          y: '50%'
+        },
+        rotation: {
+          x: '0deg',
+          y: '0deg'
+        },
+        transition: 'none',
+      }
+    }
+  }
+
+  console.log(configs)
+
+  const sheet = new ServerStyleSheet()
+
+  try {
+    const html = renderToString(sheet.collectStyles(Card(configs)))
+    const styles = sheet.getStyleTags()
+
+    res.setHeader('Content-Type', 'image/svg+xml')
+
+    return res.status(200).send(`
+      <svg
+        width="384"
+        height="552"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink">
+
+        <foreignObject width="384" height="552" style="perspective: 2000px;">
+          <div xmlns="http://www.w3.org/1999/xhtml">
+            ${styles}
+            ${html}
+          </div>
+        </foreignObject>
+      </svg>
+    `)
+  } catch (e) {
+    console.log(e)
+  } finally {
+    sheet.seal()
+  }
+
+  res.status(200).json({ name: 'John Doe' })
+}
+
+const getImage = async (url: string) => {
+  return new Promise<string>(resolve => {
+    const req = get(url, res => {
+      res.setEncoding('base64')
+      if (res.statusCode !== 200) {
+        resolve(null)
+      }
+
+      let chunk = ''
+      res.on('data', d => {
+        chunk += d
+      })
+
+      res.on('end', () => {
+        resolve('data:' + res.headers['content-type'] + ';base64,' + chunk)
+      })
+    })
+
+    req.end()
+  })
+}
