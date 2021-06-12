70c2d424a597ed8bfe7449eee1e547337c356dab
Costa <victorcosta37@gmail.com>
2021-06-12 00:09:20 -0300

    chore: removed state from card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,81 +1,34 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { CardContainer } from '../../styles/components/Card';
 
-interface CardComponentProps {
-  holoEffect: boolean,
-  sparklesEffect: boolean,
-  color: {
+export interface CardComponentProps {
+  configs: {
+    holoEffect: boolean,
+    sparklesEffect: boolean,
+    name: string,
+    description: string,
+    imgSrc: string,
     backgroundColor: string,
-    innerBackgroundColor: string
-  },
-  cardName: string,
-  cardDescription: string,
-  cardImgSrc: string
+    innerBackgroundColor: string,
+    holoPosition: any,
+    enableAnimation: boolean
+  }
 }
 
 const Card: React.FC<CardComponentProps> = props => {
-  const [holoPosition, setHoloPosition] = useState(
-    {
-      position: {
-        x: '50%',
-        y: '50%'
-      },
-      rotation: {
-        x: '0deg',
-        y: '0deg'
-      },
-      transition: 'none',
-    })
-
-  const registerMouse = ({ nativeEvent: { offsetX, offsetY }, target: { offsetHeight, offsetWidth } }) => {
-    const calcPos = (offset: number, size: number) => Math.abs(Math.floor(100 / size * offset) - 100)
-
-    const xPos = calcPos(offsetX, offsetWidth)
-    const yPos = calcPos(offsetY, offsetHeight)
-
-    const xRot = (yPos - 50) / 2;
-    const yRot = ((xPos - 50) * .5) * -1;
-
-    setHoloPosition(() => ({
-      position: {
-        x: `${xPos}%`,
-        y: `${yPos}%`
-      },
-      rotation: {
-        x: `${xRot}deg`,
-        y: `${yRot}deg`
-      },
-      transition: 'none'
-    }))
-  }
-
-  const resetPosition = () => {
-    setHoloPosition(() => ({
-      position: {
-        x: '50%',
-        y: '50%'
-      },
-      rotation: {
-        x: '0deg',
-        y: '0deg'
-      },
-      transition: '.2s'
-    }))
-  }
-
   return (
     <CardContainer
-      effects={{ holoEffect: props.holoEffect, sparklesEffect: props.sparklesEffect }}
-      colors={props.color}
-      onMouseLeave={resetPosition}
-      holoPosition={holoPosition}
-      onMouseMove={e => registerMouse(e)} >
+      effects={{ holoEffect: props.configs.holoEffect, sparklesEffect: props.configs.sparklesEffect }}
+      innerBackgroundColor={props.configs.innerBackgroundColor}
+      backgroundColor={props.configs.backgroundColor}
+      holoPosition={props.configs.holoPosition}
+      enableAnimation={props.configs.enableAnimation}
+    >
 
-      <img src={props.cardImgSrc} />
-      <span className={'name'}>{props.cardName}</span>
-      <span className={'description'}>{props.cardDescription}</span>
-    </CardContainer>
-  )
+      <img src={props.configs.imgSrc} />
+      <span className={'name'}>{props.configs.name}</span>
+      <span className={'description'}>{props.configs.description}</span>
+    </CardContainer>)
 }
 
 export default Card
