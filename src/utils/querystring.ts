ace9cd72dc8e09fedeefb3749d553eedf9aa586f
Costa <victorcosta37@gmail.com>
2021-06-20 23:48:42 -0300

    chore: add reverse querystring

--- src/utils/querystring.ts
+++ src/utils/querystring.ts
@@ -15,3 +15,39 @@ export const querystring = (obj: any, prefix?: string): string => {
 
   return str.join('&')
 }
+
+export const reverseQuerystring = (value: string, prefix?: string): any => {
+  const params = decodeURIComponent(value).split('?')[1] ?? `${prefix}=${value}`
+
+  if (params && params.length > 0) {
+    const paramsAndValues = params.split('&')
+
+    const parsed = {}
+    paramsAndValues.forEach(paramAndValue => {
+      const [key, value] = paramAndValue.split('=')
+
+      if (key.includes('[')) {
+        const subPrefixIndex = key.match(/\[/).index
+
+        const prefix = key.substring(0, subPrefixIndex)
+        const subPrefix = key.substring(subPrefixIndex + 1, key.length - 1)
+
+        parsed[prefix] = {
+          ...parsed[prefix],
+          ...reverseQuerystring(value, subPrefix)
+        }
+
+        return
+      }
+
+      if (value === 'true' || value === 'false') {
+        parsed[key] = value === 'true'
+        return
+      }
+
+      parsed[key] = value
+    })
+
+    return parsed
+  }
+}
