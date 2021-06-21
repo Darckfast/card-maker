f845a5aa48485711bb36f86a1e69b73aa162184a
Costa <victorcosta37@gmail.com>
2021-06-20 23:48:53 -0300

    chore: updated api

--- src/pages/api/card/index.ts
+++ src/pages/api/card/index.ts
@@ -3,50 +3,25 @@ import { ServerStyleSheet } from 'styled-components'
 import { renderToString } from 'react-dom/server'
 import Card from '../../../components/card/Card'
 import { get } from 'https'
+import { querystring, reverseQuerystring } from '../../../utils/querystring'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const configs = {
-    name: req.query.name as string,
-    description: req.query.description as string,
-    imgSrc: req.query.imgSrc as string,
-    holo: {
-      enabled: req.query['holo[enabled]'] === 'true',
-      src: req.query['holo[src]'] as string
-    },
-    sparkles: {
-      enabled: req.query['sparkles[enabled]'] === 'true',
-      src: req.query['sparkles[src]'] as string
-    },
-    backgroundColor: req.query.backgroundColor as string,
-    innerBackgroundColor: req.query.innerBackgroundColor as string,
-    enableAnimation: true,
-    holoPosition: {
-      position: {
-        x: '50%',
-        y: '50%'
-      },
-      rotation: {
-        x: '0deg',
-        y: '0deg'
-      },
-      transition: 'none'
-    }
+  const params = reverseQuerystring('?' + querystring(req.query))
+
+  if (params.holo.enabled) {
+    params.holo.src = await getImageAsBase64(params.holo.src as string)
   }
 
-  if (configs.holo.enabled) {
-    configs.holo.src = await getImageAsBase64(configs.holo.src as string)
-  }
-
-  if (configs.sparkles.enabled) {
-    configs.sparkles.src = await getImageAsBase64(
-      configs.sparkles.src as string
-    )
+  if (params.sparkles.enabled) {
+    params.sparkles.src = await getImageAsBase64(params.sparkles.src as string)
   }
 
   const sheet = new ServerStyleSheet()
 
   try {
-    const html = renderToString(sheet.collectStyles(Card({ configs })))
+    const html = renderToString(
+      sheet.collectStyles(Card({ ...params, enableNaturalAnimation: true }))
+    )
     const styles = sheet.getStyleTags()
 
     res.setHeader('Content-Type', 'image/svg+xml')
