d0b5f37fb5c05e8d76341fca80be80cccde4f0af
Costa <victorcosta37@gmail.com>
2021-06-22 23:18:53 -0300

    chore: unested css

--- src/styles/components/Form.ts
+++ src/styles/components/Form.ts
@@ -3,44 +3,44 @@ import styled from 'styled-components'
 export const FormContainer = styled.form`
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   flex-direction: column;
+`
+export const LabelInput = styled.label`
+  align-self: flex-end;
 
-  label {
-    align-self: flex-start;
+  input[type='text'] {
+    transition: 0.2s;
+    background-color: transparent;
+    outline: none;
+    border: 2px solid ${props => props.theme.colors.text};
+    border-radius: 4px;
+    height: 2rem;
+    width: 15rem;
+    color: ${props => props.theme.colors.text};
+    padding: 1rem;
+    margin: 0 0 1rem 1rem;
 
-    input[type='text'] {
-      transition: 0.2s;
-      background-color: transparent;
-      outline: none;
-      border: 2px solid ${props => props.theme.colors.text};
-      border-radius: 4px;
-      height: 2rem;
-      width: 15rem;
-      color: ${props => props.theme.colors.text};
-      padding: 1rem;
-      margin: 0 0 1rem 1rem;
-
-      &:disabled {
-        border: 2px solid ${props => props.theme.colors.disabledText};
-        color: grey;
-      }
-
-      &::placeholder {
-        font-family: Roboto, sans-serif;
-        font-weight: 400;
-      }
+    &:disabled {
+      border: 2px solid ${props => props.theme.colors.disabledText};
+      color: grey;
     }
 
-    input[type='color'] {
-      width: 10rem;
-      height: 2rem;
-
-      outline: none;
-      background-color: transparent;
+    &::placeholder {
+      font-family: Roboto, sans-serif;
+      font-weight: 400;
     }
   }
+
+  input[type='color'] {
+    width: 10rem;
+    height: 2rem;
+
+    outline: none;
+    background-color: transparent;
+  }
 `
+
 export const FormBackground = styled.div`
   display: flex;
   justify-content: center;
