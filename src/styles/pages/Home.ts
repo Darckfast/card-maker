d82ad40016ea879a31024ee8d185949e829ab7f0
Costa <victorcosta37@gmail.com>
2021-06-22 23:19:05 -0300

    chore: css changes

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -10,9 +10,14 @@ export const Container = styled.div`
 
   main {
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    max-width: 768px;
+    grid-template-columns: auto 320px auto;
     grid-column-gap: 1rem;
+    padding: 1rem;
+    border-radius: 20px;
+    transition: 0.2s;
+
+    box-shadow: 20px 20px 60px ${props => props.theme.colors.shadows.darkShadow},
+      -20px -20px 60px ${props => props.theme.colors.shadows.lightShadow};
 
     .toggle-button {
       align-self: flex-end;
